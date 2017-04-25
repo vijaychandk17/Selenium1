fba7300bf3ea7251d0d55573507ce88cb91496cf
Vijay <vijayachandk17@gmail.com>
2017-04-25 00:29:42 -0500

    Guru 99 GIT test demo

---
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,19 @@
+package guru99.gittest;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+
+ 
+ @Test
+  public void browserLaunch()
+ {
+  System.out.println("TestNG testing");
+
+ }
+ 
+ 
+ 
+	
+}
